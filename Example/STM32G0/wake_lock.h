28c317fe2a2b509a3de96c4d31f3334791a0d35a
zhenya.yang <zhenya.y@lx-group.com.au>
2024-05-03 12:37:53 +1000

    update wake_lock.h

--- Example/STM32G0/wake_lock.h
+++ Example/STM32G0/wake_lock.h
@@ -10,22 +10,17 @@ extern "C" {
 #ifdef USE_WAKE_LOCK
 // For every C/C++ source file which include this header, module_wake_lock static variable will be created, to describe the lock used on that module
 // In general, ACQUIRE_WAKELOCK() and RELEASE_WAKELOCK() should be appeared in pairs in the same file, not from another file function, as the module_wake_lock variable will be different
-static bool module_wake_lock = false;
+static uint8_t module_wake_lock = 0;
 
-#define ACQUIRE_WAKELOCK() {   if(!module_wake_lock){\
-                                    module_wake_lock = true;\
-                                    DEBUG_PRINTF("%s():", __FUNCTION__);\
-                                    AppFramework_WakeLockRecursive(true);\
-                                }\
-                                else\
-                                {\
-                                    DEBUG_PRINTF("Acquire WakeLock Error! %s\n", __FUNCTION__);\
-                                }\
+#define ACQUIRE_WAKELOCK() {\
+                                module_wake_lock ++;\
+                                DEBUG_PRINTF("%s():", __FUNCTION__);\
+                                AppFramework_WakeLockRecursive(true);\
                             }
-#define RELEASE_WAKELOCK() {   if(module_wake_lock){\
+#define RELEASE_WAKELOCK() {   if(module_wake_lock > 0){\
                                     DEBUG_PRINTF("%s():", __FUNCTION__);\
                                     AppFramework_WakeLockRecursive(false);\
-                                    module_wake_lock = false;\
+                                    module_wake_lock --;\
                                 }\
                                 else\
                                 {\
